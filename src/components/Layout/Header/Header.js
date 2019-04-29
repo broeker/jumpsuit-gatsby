22a4ffb391bf52505afa1b40b22851e1cc52150d
broeker <tim@electriccitizen.com>
2019-04-29 00:34:32 -0500

    JS-00: a11y.

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -20,7 +20,7 @@ function Header(props) {
       <Headroom className={styles.headroom}>
           <AppBar position="static" color="primary">
               <Toolbar>
-                  <Typography variant="h3" className={styles.logo}>
+                  <Typography variant="h4" className={styles.logo}>
                       <Link to={"/"}>{props.title}</Link>
                   </Typography>
               </Toolbar>
