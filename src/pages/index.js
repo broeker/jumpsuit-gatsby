9290b18a816d373689f18bd2495ba992bd5f2b26
broeker <tim@electriccitizen.com>
2019-04-29 20:46:06 -0500

    JS-00: homepage grid port.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Layout from '../components/Layout/Layout';
+import React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout/Layout'
+import { withStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import moment from 'moment'
+import BlogCard from '../components/BlogCard/BlogCard'
+import LinkBlog from '../components/LinkBlog/LinkBlog'
+import jumpsuit from '../images/jumpsuit01.svg'
+import JumpsuitTeaser from '../components/JumpsuitTeaser/JumpsuitTeaser'
+import 'typeface-roboto';
 import withRoot from '../withRoot';
+
+import Img from 'gatsby-image';
 
 const styles = theme => ({
   root: {
@@ -12,36 +21,168 @@ const styles = theme => ({
 });
 
 class Index extends React.Component {
-  state = {
-    open: false,
-  };
+  renderElement() {
+    const { classes } = this.props;
+    const Home = () => <><img className={classes.img} alt="jumpsuit" src={jumpsuit} /></>
+    if (  this.props.data  ) {
+      return (
+          <div>
+            <Grid container spacing={24}>
+              <Grid item lg={8}>
+                <Grid container spacing={24}>
+                  { this.props.data.allNodeBlog.edges.map(({ node: blog }, key) => {
 
-  handleClose = () => {
-    this.setState({
-      open: false,
-    });
-  };
+                        var grid;
 
-  handleClick = () => {
-    this.setState({
-      open: true,
-    });
-  };
+                        if (key === 0) {
+                          grid=12
+                        } else {
+                          grid=6
+                        }
+
+                        var media;
+                        if (blog.relationships.field_hero) {
+                          media = blog.relationships.field_hero.relationships.field_media_image.localFile.childImageSharp.fluid
+                        } else {
+                          media = ''
+                        }
+
+
+                        return (
+                            <>
+
+                              <Grid className={classes.card} item key={blog.title} lg={grid}>
+                                <BlogCard
+                                    title={blog.title}
+                                    summary={blog.summary.processed}
+                                    category={blog.relationships.category[0].name}
+                                    path={blog.fields.slug}
+                                    media={media}
+                                    changed={moment(blog.changed).format('MMMM DD, YYYY')}
+                                />
+                              </Grid>
+                            </>
+                        );
+                      }
+                  )
+                  }
+                </Grid>
+              </Grid>
+              <Grid item lg={4}>
+                <JumpsuitTeaser />
+
+                <LinkBlog />
+
+              </Grid>
+              <Grid item lg={12}>
+
+                <div className={classes.inlineheader}>JUMPSUIT 101</div>
+                <Home />
+              </Grid>
+            </Grid>
+
+          </div>
+      );
+    }
+  }
 
   render() {
-    const { classes } = this.props;
-    const { open } = this.state;
-
     return (
         <Layout>
-        GRID
+          <Grid container spacing={24}>
+            <>
+              { this.renderElement() }
+            </>
+          </Grid>
         </Layout>
-    );
+    )
   }
 }
 
-Index.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
 export default withRoot(withStyles(styles)(Index));
+
+export const query = graphql`
+  query {
+   imageOne: file(relativePath: { eq: "doit.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 1000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+   } 
+    imageTwo: file(relativePath: { eq: "rocket.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 1000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    } 
+    safetyfirst: file(relativePath: { eq: "safety_first.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 400) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    allNodeBlog(
+      sort: {
+        fields: [changed], order:DESC
+      }
+      limit: 3
+      ) 
+      {
+      edges {
+        node {
+          fields {
+            slug 
+          }
+          id
+          title
+          created
+          changed
+          field_featured
+          summary: field_summary {
+            format
+            processed
+          }
+          relationships {
+            category: field_blog_category {
+              name,
+            },
+            tags: field_tags {
+            name,
+            },
+            field_hero {
+              relationships {
+                field_media_image {
+                   filename
+                    uri {
+                      value
+                      url
+                      }
+                  localFile {
+                  publicURL
+                  childImageSharp {
+                     fluid(
+                      maxHeight: 548, 
+                      maxWidth: 1280, 
+                      ) 
+                    {
+                     ...GatsbyImageSharpFluid
+                      aspectRatio,
+                      presentationWidth,
+                      presentationHeight,
+                    }
+                  }
+                }
+                }
+              }
+            },
+          }
+          created
+        }
+      }
+    }
+  }
+`;
+
